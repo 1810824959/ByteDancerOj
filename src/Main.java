b465ee857c48adf227240b50acd0d355df9bdfd4
liyang <leesein@outlook.com>
2018-12-16 23:04:32 +0800

    12/16 使用 stack实现简化路径

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,20 @@
 import String.Subject1;
 
+import java.util.Stack;
+
 public class Main {
 
     public static void main(String[] args) {
         Subject1 subject1 = new Subject1();
+        subject1.test();
+//        Stack<Integer> s = new Stack<>();
+//        s.push(1);
+//        s.push(2);
+//        s.push(3);
+//        Integer x = s.pop();
+//        System.out.println(x);
 //        System.out.println(subject1.lengthOfLongestSubstring("pwwkew"));
-        System.out.println(subject1.test2("pwwkew"));
+//        System.out.println(subject1.test2("pwwkew"));
+//        System.out.println(subject1.simplifyPath("/a/./b/../../c/"));
     }
 }
--- src/String/Subject1.java
+++ src/String/Subject1.java
@@ -1,6 +1,6 @@
 package String;
 
-import java.util.ArrayList;
+import java.util.*;
 
 /**
  * 找出其中不含有重复字符的 最长子串 的长度
@@ -44,5 +44,59 @@ public class Subject1 {
             array[s.charAt(i)] = i + 1;
         }
         return ans;
+    }
+
+    /**
+     * 简化路径
+     * @param path
+     * @return
+     */
+    public String simplifyPath(String path) {
+        // Write your code here
+        String s = path.replaceAll("//", "/");
+        String[] Names = s.split("/");
+        Stack<String> S = new Stack<String>();
+        for (int i = 0; i < Names.length; ++i) {
+            if (".".equals(Names[i]) || "".equals(Names[i]))
+                continue;
+            if ("..".equals(Names[i])) {
+                if (!S.empty())   //考虑了边界，，不能在空的时候 pop
+                    S.pop();
+            } else
+                S.push(Names[i]);
+        }
+        StringBuilder R = new StringBuilder();
+        while (!S.empty()) {
+            R.insert(0, S.pop());
+            R.insert(0, "/");
+        }
+        if (R.length() == 0)
+            R.append("/");
+        return R.toString();
+    }
+
+    public void test(){
+        TreeSet ts=new TreeSet(new CompareByLen());
+        ts.add("orange");
+        ts.add("apple");
+        ts.add("banana");
+        ts.add("grape");
+
+
+        Iterator it=ts.iterator();
+        while(it.hasNext())
+        {
+            String fruit=(String)it.next();
+            System.out.println(fruit);
+        }
+    }
+}
+
+class CompareByLen /*extends Object*/ implements Comparator<String> {
+
+    @Override
+    public int compare(String s1, String s2) {        //按照字符串的长度比较
+        int num = s1.length() - s2.length();        //长度为主要条件
+        return num == 0 ? s1.compareTo(s2) : num;    //内容为次要条件
     }
 }
