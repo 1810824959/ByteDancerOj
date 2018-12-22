50e895426aa13e70aa1805ece5ea81dd9ff9b83f
liyang <leesein@outlook.com>
2018-12-22 16:37:14 +0800

    12/19 朋友圈那个用并查集，新的翻转链表使用迭代和递归两种方法

--- src/Array/CircleNum.java
+++ src/Array/CircleNum.java
@@ -1,7 +1,12 @@
 package Array;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CircleNum {
-    public int findCircleNum(int[][] M) {
+    private List<List<Integer>> lists = new ArrayList<>();
+
+    public int findCircleNum_1(int[][] M) {
         int num = 0;
         for (int i = 0 ; i<M.length ; i++){
             for (int j = 0 ; j<M[0].length ; j++){
@@ -26,6 +31,40 @@ public class CircleNum {
         System.out.println("下");
         generate(M,i,j+1);
         generate(M,i,j-1);
+    }
+
+    public int findCircleNum(int[][] M) {
+        return method2(M);
+    }
+    /*
+        初始时每个个体的根朋友为自己本身，若M[i][j] == 1，说明i 和 j是朋友，找到i的根朋友i_parent和j的根朋友j_parent，
+        若两者根朋友一样说明是一个朋友圈，不做操作；若两者根朋友不一样，说明是不同的朋友圈，则执行合并朋友圈的操作，将j的根朋友
+        j_parent的根朋友设置为i的根朋友i_parent，总数减一
+    */
+    public int method2(int[][] M){
+        int count = M.length;
+        int[] help = new int[M.length];
+        for(int i = 0;i < help.length;i++)
+            help[i] = i;
+        for(int i = 0;i < M.length;i++){
+            for(int j = i + 1;j < M.length;j++){
+                if(M[i][j] == 1){
+                    int i_parent = findParent(help,i);
+                    int j_parent = findParent(help,j);
+                    if(i_parent != j_parent){
+                        help[j_parent] = i_parent;
+                        count--;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+    public int findParent(int[] help,int id){
+        if(help[id] == id)
+            return id;
+        return findParent(help,help[id]);
     }
 
     public static void main(String[] args) {
---
+++ src/Array/RainTrap.java
@@ -0,0 +1,13 @@
+package Array;
+
+public class RainTrap {
+    public int trap(int[] height) {
+        int rain = 0;
+        int index = 0;
+        return 0;
+    }
+
+    private void generate(int[] height,int index){
+        while (height[index]!=0){}
+    }
+}
